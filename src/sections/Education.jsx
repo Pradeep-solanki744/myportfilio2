63ffe0428f4e3b87d8c672e7da9e39c2c65d1854
Pradeep-solanki744 <pradeepsolanki2703@gmail.com>
2026-05-05 18:05:45 +0530

    ee

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -1,36 +1,6 @@
-export default function Education() {
-  const education = [
-    {
-      degree: "B.Tech Computer Science Engineering",
-      institution: "SANSKRITI UNIVERSITY",
-      duration: "2022 - 2025",
-      description: 
-      
-        "Focused on software development, data structures, algorithms, database management, and modern web technologies.",
-        
-    },
-    {
-      degree: "Computer Science & Engineering",
-      institution: "P K POLYTECHNIC",
-      duration: "2020 - 2022",
-      description:
-        "Built a strong academic foundation in Mathematics, Physics, and Computer Science.",
-    },
-    {
-      degree: "Intermediate (UP.BOARD)",
-      institution: "CH RAJVEER SINGH MIC ANORA MATHURA",
-      duration: "2019 - 2020",
-      description:
-        "Built a strong academic foundation in Mathematics, Physics, and  Chemistry.",
-    }, 
-     {
-      degree: "High School (UP.BOARD)",
-      institution: "BABA H SINGH H S S JARELIYA RAYA MATHURA",
-      duration: "2017 - 2018",
-      description:
-        "Built a strong academic foundation in Mathematics.",
-    }, 
-  ];
+import { EDUCATION } from "../constants/data";
+
+export const Education = () => {
   return (
     <section id="education" className="py-20 px-4">
       <div className="max-w-6xl mx-auto">
@@ -44,7 +14,7 @@ export default function Education() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          {education.map((item, index) => (
+          {EDUCATION.map((item, index) => (
             <div
               key={index}
               className="rounded-2xl border border-gray-100 shadow-md hover:shadow-xl transition-all duration-300 p-8"
@@ -70,4 +40,6 @@ export default function Education() {
       </div>
     </section>
   );
-}
+};
+
+export default Education;
